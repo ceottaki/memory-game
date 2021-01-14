76b9c873ddd81e4fca80a97ff77ffb1bf332b383
Felipe Ceotto <felipe.ceotto@gamingmechanics.com>
2021-01-14 15:29:13 +0000

    App icons.

--- app/src/pages/index.tsx
+++ app/src/pages/index.tsx
@@ -6,7 +6,7 @@ import MainLayout from '../components/MainLayout/MainLayout'
 
 const Home: NextPage = () => {
   return (
-    <MainLayout>
+    <MainLayout metaTitle='Play Memory Game!' siteTitle='Memory Game'>
       <Board height={4} width={5} />
     </MainLayout>
   )
